9a503fe38dc220ce35158c5a5ffdcf6e482605f8
K0T0Let <frolov.666@yandex.ru>
2022-06-16 21:46:13 +0300

    Мелкие изменения

--- MyShop/data/ModelViews/MVElement.cs
+++ MyShop/data/ModelViews/MVElement.cs
@@ -27,24 +27,32 @@ namespace MyShop.data.ModelViews
 
         void Sets(Assembly assembly, IEnumerable<Assembly> assemblies)
         {
+            //Id сборки
             assemblyId = assembly.Id;
 
+            //Цена сборки
             price = (assembly.Connections.FirstOrDefault(c => c.PropertyId == 2).Meaning).ToDouble();
 
+            //Полное название сборки
             var ModelName = new System.Text.StringBuilder();
             ModelName.Append(assembly.Product.Name);
             ModelName.Append($" {assembly.Connections.FirstOrDefault(c => c.PropertyId == 3).Meaning}Gb ");
             ModelName.Append(assembly.Color.Name);
             name = ModelName.ToString();
 
+            //Брэнд
             brand = assembly.Product.Brand;
 
+            //Количество сборкок
             quantity = int.Parse(assembly.Connections.FirstOrDefault(c => c.PropertyId == 4).Meaning);
 
+            //Описание продукта
             description = new HtmlString(assembly.Product.Description);
 
+            //Список картинок
             images.AddRange(assembly.Images.Select(i => i.Src).ToList());
 
+            //Список цветов
             colors.Add(assembly.Id, assembly.Color);
             foreach (var item in assemblies)
             {
@@ -53,6 +61,7 @@ namespace MyShop.data.ModelViews
             }
             colors = colors.OrderBy(c => c.Value.Id).ToDictionary(c => c.Key, c => c.Value);
 
+            //Список встроенной памяти
             ROMs.Add(assembly.Id, assembly.Connections.FirstOrDefault(c => c.PropertyId == 3).Meaning);
             foreach (var item in assemblies.Where(a => a.ColorId == assembly.ColorId))
             {
@@ -61,6 +70,11 @@ namespace MyShop.data.ModelViews
             }
             ROMs = ROMs.OrderBy(r => int.Parse(r.Value)).ToDictionary(r => r.Key, r => r.Value);
 
+            //Список хорактеристик
+            categories.Add(
+                assembly.Connections.FirstOrDefault(c => c.Property.Category_propertyId == 2).Property.Category_property.Name, 
+                new List<(string Name, string Value)> { (Name: "Цвет", Value: assembly.Color.Name) }
+                );
             foreach (var item in assembly.Connections)
             {
                 if (item.Property.Category_propertyId != 1)
